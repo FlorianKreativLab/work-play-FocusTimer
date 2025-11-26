6fa1b7fe7c0815a04ea9b63f117cd213a2afb2e2
FlorianKreativLab <florian.jautze@proton.me>
2025-11-26 23:41:57 +0100

    Syntaxfehler behoben

--- dayNight.js
+++
@@ -1,9 +0,0 @@
-let dayNightProgress = 0;
-
-function setDayNightProgress(value) {
-    dayNightProgress = Math.max(0, Math.min(1, value));
-}
- function getDayNightProgress() {
-    return dayNightProgress;
-}
-export { setDayNightProgress, getDayNightProgress };
